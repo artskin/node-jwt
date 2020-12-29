886a90af1853bd2bb937f8a6a1f0330b8a412c34
Guliqiang <guliqiang@sensetime.com>
2020-12-29 18:58:19 +0800

    token

---
+++ src/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const {createToken,verifyToken} = require('./token.js')
+const app = express()
+
+const secret = 'dsdsd123sdqd34'
+
+const user = {
+  id:10,
+  name:"amu",
+  age:16
+}
+
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
+  res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
+  res.setHeader("X-Powered-By",' 3.2.1')
+
+  if(req.method ==='OPTIONS') res.send(200);
+  else next();
+})
+
+const whiteList = ['/login']
+
+app.use((req,res,next)=>{
+  if(whiteList.includes(req.url)){
+    verifyToken(req.headers.authorization).then(res =>{
+      next()
+    }).catch(err=>{
+      res.status(401).send('token 无效')
+    })
+  } else {
+    next()
+  }
+})
+
+app.post('/login',(req,res)=>{
+  let token = createToken(user)
+  res.json({token})
+})
+
+app.get('/userInfo',(req,res)=>{
+  res.send({
+    result:1,
+    data:{
+      'name':'amu',
+      'id':1
+    }
+  })
+})
+
+app.listen(4000,()=>{
+  console.log('Server started on port 4000')
+})
+app.use(express.static('public'))
---
+++ src/token.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const scret = "21321dsadsadsdsd";
+
+//生成token
+const createToken = (info)=>{
+  let token = jwt.sign(info,scret,{
+    expiresIn: 60 * 60 * 10
+  })
+
+  return token
+}
+
+//验证token
+
+const verifyToken = () =>{
+  return new Promise((resolve,reject)=>{
+    jwt.verify(token,scret,(err,result)=>{
+      if(err){
+        reject(err)
+      }else{
+        resolve(result)
+      }
+    })
+  })
+}
+module.exports = {createToken,verifyToken}
