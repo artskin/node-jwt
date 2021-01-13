498a1fc95c367c5dcbe99ad9b482d815032a35ac
Guliqiang <guliqiang@sensetime.com>
2021-01-13 14:41:15 +0800

    src/app.js

--- public/app/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const usersSchema = new mongoose.Schema({
-  username:String,
-  password:String,
-  _id:String
-})
-
-module.exports = mongoose.model('User',usersSchema)
